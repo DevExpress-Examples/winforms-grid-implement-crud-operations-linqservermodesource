8531009320d2a8f18fa38cf5ef158cb85cb22998
DevExpressExampleBot <fakeEmail>
2023-09-03 13:57:00 +0400

    autogenerate VB

--- VB/LinqServerModeSource/Program.vb
+++ VB/LinqServerModeSource/Program.vb
@@ -1,4 +1,4 @@
-﻿' Developer Express Code Central Example:
+' Developer Express Code Central Example:
 ' How to implement CRUD operations using XtraGrid and LinqServeModeSource
 ' 
 ' This example demonstrates how to implement create, update and delete operations
@@ -8,30 +8,25 @@
 ' 
 ' You can find sample updates and versions for different programming languages here:
 ' http://www.devexpress.com/example=E4498
-
-Imports Microsoft.VisualBasic
 Imports System
-Imports System.Collections.Generic
 Imports System.Windows.Forms
 Imports DevExpress.LookAndFeel
 
 Namespace LinqServerModeSource
-	Friend NotInheritable Class Program
-		''' <summary>
-		''' The main entry point for the application.
-		''' </summary>
-		Private Sub New()
-		End Sub
-		<STAThread> _
-		Shared Sub Main()
-			Application.EnableVisualStyles()
-			Application.SetCompatibleTextRenderingDefault(False)
 
-			DevExpress.Skins.SkinManager.EnableFormSkins()
-			DevExpress.UserSkins.BonusSkins.Register()
-			UserLookAndFeel.Default.SetSkinStyle("Foggy")
+    Friend Module Program
 
-			Application.Run(New Form1())
-		End Sub
-	End Class
+        ''' <summary>
+        ''' The main entry point for the application.
+        ''' </summary>
+        <STAThread>
+        Sub Main()
+            Call Application.EnableVisualStyles()
+            Application.SetCompatibleTextRenderingDefault(False)
+            DevExpress.Skins.SkinManager.EnableFormSkins()
+            DevExpress.UserSkins.BonusSkins.Register()
+            UserLookAndFeel.Default.SetSkinStyle("Foggy")
+            Call Application.Run(New Form1())
+        End Sub
+    End Module
 End Namespace
